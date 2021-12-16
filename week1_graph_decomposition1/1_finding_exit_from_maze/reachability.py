69544a6d9936555edce2628423535f4b47a2c3f9
Vecti <dominczakwojciech@gmail.com>
2021-12-16 17:17:47 +0100

    Implementation of solutions to week 1

--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
@@ -1,29 +1,46 @@
-#Uses python3
-
 import sys
 
-class EzGraph():
 
+class EzGraph():
+    """
+    Implementation of graph class with basic methods.
+    Vertices of graph are represented as integers from 0 to n.
+    """
     def __init__(self, adj_input, egde_list):
         self.adj_input = adj_input
         self.adjacency_list = self.create_adjacency_list()
         self.visited = [False] * len(self.adjacency_list)
         self.vertice_list = [i for i in range(len(self.adjacency_list))]
         self.egde_list = egde_list
-        # self.egdes_list = self.create_egde_list()
+
     def create_adjacency_list(self):
+        """
+        Creates adjacency list - converts from representation of graph
+        from: list of list
+        to: dict of list
+        Returns:
+
+        --------
+        adj_list: dict
+            dict with representation of graph as adjacency list
+        """
+
         adj_list = {}
         for i, v in enumerate(self.adj_input):
             adj_list[i] = v
         return adj_list
 
-    # def create_egde_list(self):
-    #     edge_list = {}
-    #     for i, v in enumerate(self.egdes):
-    #         edge_list[i] = v
-    #     return edge_li
+    def explore(self, v: int):
+        """
+        Implementation of explore method from lecture:
+        For given vertices explore all vertices reachable from it
 
-    def explore(self, v):
+        Parameters:
+        -----------
+        v: int
+            int representing the given vertices in graph
+
+        """
         self.visited[v] = True
         for edge in self.egde_list:
             if edge[0] == v:
@@ -34,38 +51,35 @@ class EzGraph():
                     self.explore(edge[0])
 
     def DFS(self):
+        """
+        Implementation of Deep First Search Method from lecture
+        """
         for i in self.vertice_list:
             self.visited[i] = False
         for i in self.vertice_list:
             if not self.visited[i]:
-                # problem: DFS will visit every vertice - so all will be visited
-                # but we 
                 self.explore(i)
 
-    def reach(self, x, y):
+    def reach(self, x: int, y: int) -> bool:
+        """
+        Method which checks if given vertices are reachable from each other
+
+        Parameters:
+        -----------
+        x: int
+            int representing the given vertice
+        y: int
+            int representing the given vertice
+
+        Returns:
+        --------
+        1 if vertices are reachable, 0 if not
+        """
         if self.visited[x] == 1 and self.visited[y] == 1:
             return 1
         else:
             return 0
 
-
-
-# input_list = [[1, 3], [0, 2], [1, 3], [2, 0]]
-# edges = [(0, 1), (2, 1), (3, 2), (0, 3)]
-# graph = EzGraph(input_list, edges)
-# print(graph.create_adjacency_list()) 
-# print(graph.visited)
-# print(graph.vertice_list)
-# print(graph.DFS())
-# print(graph.visited)
-# print(graph.egdes)
-# print(graph.create_egde_list())
-# def reach(adj, x, y):
-#     #write your code here
-
-
-
-#     return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -73,25 +87,13 @@ if __name__ == '__main__':
     n, m = data[0:2]
     data = data[2:]
     for i in range(len(data)):
-        data[i] = data[i] -1
+        data[i] = data[i] - 1
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     x, y = data[2 * m:]
     adj = [[] for _ in range(n)]
-    # x, y = x - 1, y - 1
     for (a, b) in edges:
-        adj[a ].append(b )
-        adj[b ].append(a )
+        adj[a].append(b)
+        adj[b].append(a)
     graph = EzGraph(adj, edges)
-    # print(graph.adjacency_list)
-    # print(graph.egde_list)
-    # print(graph.visited)
-    # print(graph.vertice_list)
-    # print(x)
-    # print(y)
     graph.explore(x)
-    # print(graph.visited)
     print(graph.reach(x, y))
-#     print(adj)
-#     print(edges)
-#     print(x)
-#     print(y)
--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -1,21 +1,103 @@
-#Uses python3
-
 import sys
 
 
-def number_of_components(adj):
-    result = 0
-    #write your code here
-    return result
+class EzGraph():
+    """
+    Implementation of graph class with basic methods.
+    Vertices of graph are represented as integers from 0 to n.
+    """
+    def __init__(self, adj_input, egde_list):
+        self.adj_input = adj_input
+        self.adjacency_list = self.create_adjacency_list()
+        self.visited = [False] * len(self.adjacency_list)
+        self.vertice_list = [i for i in range(len(self.adjacency_list))]
+        self.egde_list = egde_list
+
+    def create_adjacency_list(self):
+        """
+        Creates adjacency list - converts from representation of graph
+        from: list of list
+        to: dict of list
+        Returns:
+
+        --------
+        adj_list: dict
+            dict with representation of graph as adjacency list
+        """
+
+        adj_list = {}
+        for i, v in enumerate(self.adj_input):
+            adj_list[i] = v
+        return adj_list
+
+    def explore(self, v: int):
+        """
+        Implementation of explore method from lecture:
+        For given vertices explore all vertices reachable from it
+
+        Parameters:
+        -----------
+        v: int
+            int representing the given vertices in graph
+
+        """
+        self.visited[v] = True
+        for edge in self.egde_list:
+            if edge[0] == v:
+                if not self.visited[edge[1]]:
+                    self.explore(edge[1])
+            elif edge[1] == v:
+                if not self.visited[edge[0]]:
+                    self.explore(edge[0])
+
+    def DFS(self):
+        """
+        Implementation of Deep First Search Method from lecture.
+        In addition of counting component components:
+        """
+        self.cc = 0
+        for i in self.vertice_list:
+            self.visited[i] = False
+        for i in self.vertice_list:
+            if not self.visited[i]:
+                self.cc += 1
+                self.explore(i)
+
+    def reach(self, x: int, y: int) -> bool:
+        """
+        Method which checks if given vertices are reachable from each other
+
+        Parameters:
+        -----------
+        x: int
+            int representing the given vertice
+        y: int
+            int representing the given vertice
+
+        Returns:
+        --------
+        1 if vertices are reachable, 0 if not
+        """
+        if self.visited[x] == 1 and self.visited[y] == 1:
+            return 1
+        else:
+            return 0
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
+    for i in range(len(data)):
+        data[i] = data[i] - 1
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    # x, y = data[2 * m:]
     adj = [[] for _ in range(n)]
+    # x, y = x - 1, y - 1
     for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(number_of_components(adj))
+        adj[a].append(b)
+        adj[b].append(a)
+    graph = EzGraph(adj, edges)
+    graph.DFS()
+    print(graph.cc)
