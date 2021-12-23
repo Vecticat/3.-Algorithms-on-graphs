ffcd7f922946621d894f76e1b50dcc6959c1f520
Vecticat <wdominczaklearn@gmail.com>
2021-12-23 12:34:41 +0100

    Push

--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
@@ -5,6 +5,7 @@ class EzGraph():
     """
     Implementation of graph class with basic methods.
     Vertices of graph are represented as integers from 0 to n.
+    Hello
     """
     def __init__(self, adj_input, egde_list):
         self.adj_input = adj_input
